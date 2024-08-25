27645b4973d7d9d190e83237a4f8c76521019638
Anurag Kumar <88237080+anuragK-24@users.noreply.github.com>
2024-08-25 13:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-# hkjkjh
+
 @app.route("/")
 def hello():
-   return "Hello Anurag!!"
+   return "Hello Anurag Kumar!!"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
